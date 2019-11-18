ac14afdf7b498c9a8883337be45e8243dcb5514c
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-18 00:48:31 -0300

    executando o algoritmo de agrupamento

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import pandas as pd
+import pandas as pd     #Me dá modelos .csv
+from sklearn.cluster import KMeans      #permite a execução do algoritmo de cluster
 
 iris = pd.read_csv('iris.csv')  #Puxando arquivo .csv para utilizá-lo
 init = iris.head()              #Exibindo os 5 primeiros valores
-ult = iris.tail()           #Exibindo os 5 últimos
+ult = iris.tail()           #Exibindo os 5 últimos'
 
-x = iris.iloc[:,0:4].values
+x = iris.iloc[:, 0:4].values
 
-print(x)
-
+#1º Numero de clusters  2º Forma como ele é gerado
+kmeans = KMeans(n_clusters=3, init='random')
+print(kmeans.fit(x))       #Executando o algoritmo e agrupando os dados
 
