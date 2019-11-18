358b36de4b8442a45600a2995dfdc714f71f20b1
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-18 00:27:21 -0300

    Utilizando a biblioteca 'pandas'

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-iris = pd.read_csv('iris.csv')
-init = iris.head()
-ult = iris.tail()
+iris = pd.read_csv('iris.csv')  #Puxando arquivo .csv para utilizá-lo
+init = iris.head()              #Exibindo os 5 primeiros valores
+ult = iris.tail()           #Exibindo os 5 últimos
 
-print(init)
+x = iris.iloc[:,0:4].values
+
+print(x)
 
 
