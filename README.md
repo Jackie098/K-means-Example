09d068c41a4d2de7a35add82086bd4bba08d33cd
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-18 22:56:38 -0300

    visualizando a distancia de cada dado em relação a todos os clusters

---
+++ README.md
@@ -0,0 +1,7 @@
+Utilizando a biblioteca 'PANDAS' para importar 
+e ler dados tabelados (extensão '.csv'), foi testado
+o uso do algoritmo de agrupamento (clusters)
+K-means. Dado como exemplo a tabela "iris" (informações
+sobre diversos tipos de flores), foi mostrado algumas funcionalidades
+da biblioteca 'sklearn.cluster' que importa o 
+K-means
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import pandas as pd     #Me dá modelos .csv
-from sklearn.cluster import KMeans      #permite a execução do algoritmo de cluster
+import pandas as pd  # Me dá modelos .csv
+from sklearn.cluster import KMeans  # permite a execução do algoritmo de cluster
 
-iris = pd.read_csv('iris.csv')  #Puxando arquivo .csv para utilizá-lo
-init = iris.head()              #Exibindo os 5 primeiros valores
-ult = iris.tail()           #Exibindo os 5 últimos'
+iris = pd.read_csv('iris.csv')  # Puxando arquivo .csv para utilizá-lo
+init = iris.head()  # Exibindo os 5 primeiros valores
+ult = iris.tail()  # Exibindo os 5 últimos
 
 x = iris.iloc[:, 0:4].values
 
+# Método de inicialização dos clusters
+kmeans = KMeans(n_clusters=3, init='random')  # 1º Numero de clusters  2º Forma como ele é gerado
 
-#Método de inicialização dos clusters
-kmeans = KMeans(n_clusters=3, init='random')    #1º Numero de clusters  2º Forma como ele é gerado
+# Executando o algoritmo e agrupando os dados
+print("\nFIT -> ")
+print (kmeans.fit(x))
 
-#Executando o algoritmo e agrupando os dados
-print(kmeans.fit(x))
-
-#Verifica e mostra os centroids gerados
+# Verifica e mostra os centroids gerados
+print("\nCLUSTER_CENTERS_ -> ")
 print(kmeans.cluster_centers_)
+
+# Agrupar os dados e retornar uma tabela de distancias. Esta tabela e criada de forma que
+# cada instancia contem os valores da distancia em relacao a cada cluster
+distance = kmeans.fit_transform(x)
+print("\nFIT_TRANSFORM -> ")
+print(distance)
+
+
