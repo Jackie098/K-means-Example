c5bb87eff9a59ceb3ae39870ef1b761eb584f859
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-20 16:16:54 -0300

    fix: exibir centroids

--- viewClusters.py
+++ viewClusters.py
@@ -7,11 +7,13 @@ kmeans = KMeans(n_clusters=3, init='random')
 iris = pd.read_csv('iris.csv')
 x = iris.iloc[:, 0:4].values
 
-plt.scatter(x[:, 0], x[:, 1], s = 100, c = kmeans.labels_)
+kmeans.fit(x)
+
+plt.scatter(x[:, 0], x[:, 1], s=100, c=kmeans.labels_)
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'red', label = 'Centroids')
 plt.title('Iris Clusters and Centroids')
 plt.xlabel('SepalLength')
-plt.ylabel('SepalWIdth')
+plt.ylabel('SepalWidth')
 plt.legend()
 
 plt.show()
